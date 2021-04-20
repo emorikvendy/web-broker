cf4e7ac93d164b8bdbb3e62337f74b40dfbb514c
eol morikvendy <emorikvendy@gmail.com>
2021-04-20 19:36:31 +0300

    Изменить структуру типового веб-сервиса так, чтобы любое хранилище можно было создавать через New, а не через NewPgRepo и тд.

--- internal/pkg/repository/repo.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/emorikvendy/web-broker/internal/pkg/model"
-
-type repo interface {
-	Get(getReq string) (string, error)
-	Put(putReq *model.PutValue) error
-}
